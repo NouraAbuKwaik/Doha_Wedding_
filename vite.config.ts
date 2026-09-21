b85a1524ec2e982a02f757634a1208802964598c
Noura  AbuKwaik <noura.abukwaik@ivalueconsult.com>
2026-09-21 15:32:27 +0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Ahmad-Wedding/",
+base: "/Doha_Wedding_/",
 
   plugins: [
     react(),
